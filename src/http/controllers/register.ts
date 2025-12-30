6b0dcaff40171a196c1026f7ca0f3a9e2e6197ef
Renato Alves <renatoalves1610@gmail.com>
2025-12-29 22:29:07 -0300

    feat: add register controller and routes

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,22 @@
+import type { FastifyRequest, FastifyReply } from "fastify"
+import { prisma } from "lib/prisma"
+import z from "zod"
+
+export async function register (request: FastifyRequest, reply: FastifyReply) {
+  const registerBodyShema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    })
+    const { name, email, password } = registerBodyShema.parse(request.body)
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password_hash: password,
+      },
+    })
+
+    return reply.status(201).send()
+}

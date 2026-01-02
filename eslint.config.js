3963a1aefbc0cf5a91478e70c0eba510efb6c635
Renato Alves <renatoalves1610@gmail.com>
2026-01-01 22:15:59 -0300

    test: implement authenticate use case unit tests

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default tseslint.config(
       },
     },
     rules: {
-      'no-unused-vars': 'off',
+      // 'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': ['error'],
     },
   },

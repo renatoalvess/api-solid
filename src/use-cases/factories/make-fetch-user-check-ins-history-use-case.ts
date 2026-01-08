d8aa691c8559cee362ff4eefc168a9a307811c57
Renato Alves <renatoalves1610@gmail.com>
2026-01-08 17:22:44 -0300

    feat: create factories for all use cases

---
+++ src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -0,0 +1,9 @@
+import { FetchUserChechInsHistoryUseCase } from '../fetch-user-check-ins-history'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+
+export function makeFetchUserCheckInsHistoryUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const useCase = new FetchUserChechInsHistoryUseCase(checkInsRepository)
+  
+  return useCase
+}
